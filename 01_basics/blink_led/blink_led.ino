8657e33a58cec30ac5f3bcdc7171afd7ab96f980
Blevy <leverianbeatus@gmail.com>
2026-07-01 15:53:36 +0000

    Update blink_led.ino

--- 01_basics/blink_led/blink_led.ino
+++ 01_basics/blink_led/blink_led.ino
@@ -1,4 +1,36 @@
+/*
+ * Blink LED - Enhanced Version
+ * Embedded Systems Lab - 01_basics
+ * 
+ * Demonstrates digital output with non-blocking timing using millis()
+ */
+
+const int LED_PIN = 13;           // Most Arduino boards have built-in LED on pin 13
+unsigned long previousMillis = 0;
+const long interval = 1000;       // Blink interval (ms)
+
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);             // For debugging
+  Serial.println("Blink LED Started - Enhanced Version");
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  // Non-blocking blink
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    // Toggle LED
+    static bool ledState = false;
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+
+    Serial.print("LED State: ");
+    Serial.println(ledState ? "ON" : "OFF");
+  }
+}void setup() {
   pinMode(13, OUTPUT);
 }
 
